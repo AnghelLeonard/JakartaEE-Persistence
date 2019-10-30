5e551f1b76dc4fc68cf1d61f636051929b5aa4f6
AnghelLeonard <leoprivacy@yahoo.com>
2019-10-30 13:53:06 +0200

    Caching query results

---
+++ QueryResultsCache/README.md
@@ -0,0 +1,1 @@
+This application is an example of best mapping of a bidirectional `@OneToMany` association. 
---
+++ QueryResultsCache/src/main/java/com/bookstore/resources/BookstoreResource.java
@@ -0,0 +1,34 @@
+package com.bookstore.resources;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("bookstore")
+@Stateless
+public class BookstoreResource {
+
+    @PersistenceContext(unitName = "PU")
+    private EntityManager em;
+
+    @GET
+    @Path("load")    
+    public Response removeBookOfAuthor() {
+
+        System.out.println("\n\nLoading author with books ...");
+        
+        Query query = em
+                .createQuery("SELECT a FROM Author a JOIN FETCH a.books WHERE a.name='Joana Nimar'");       
+        query.setHint("eclipselink.cache-usage", "CheckCacheThenDatabase");
+        
+        System.out.println(query.getSingleResult());
+        
+        return Response
+                .ok("Load done! Refresh page to load again from cache ...")
+                .build();
+    }    
+}
